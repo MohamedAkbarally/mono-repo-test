66317eb4d65b92cd7ccf319060db1ff55a499c77
Mohamed <mohamed@Mohameds-MacBook-Pro-2.local>
2023-08-10 16:26:32 +1000

    workflow-test

--- module-one/main.py
+++ module-one/main.py
@@ -1,8 +1,8 @@
 from functools import partial
 from lambda_helper import lambda_handler
-from src.mock_module_one import mock_main_function
-from src.module_one import main_function
+from src.mock_module_one import mock_module_one
+from src.module_one import module_one
 
 
-main_handler = partial(lambda_handler, main_function)
-mock_handler = partial(lambda_handler, mock_main_function)
+main_handler = partial(lambda_handler, module_one)
+mock_handler = partial(lambda_handler, mock_module_one)
